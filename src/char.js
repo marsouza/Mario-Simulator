4dc1ef9232aab72895160d8110d554f7341caeed
Marcos Souza <marcos.souza@teknisa.com>
2025-06-20 22:25:59 -0300

    Incluído seleção automática de players

---
+++ src/char.js
@@ -0,0 +1,40 @@
+const characters = [
+  {
+    name: 'Mario',
+    speed: 4,
+    maneuverability: 3,
+    power: 3
+  },
+  {
+    name: 'Luigi',
+    speed: 3,
+    maneuverability: 4,
+    power: 4
+  },
+  {
+    name: 'Bowser',
+    speed: 5,
+    maneuverability: 2,
+    power: 5
+  },
+  {
+    name: 'Peach',
+    speed: 3,
+    maneuverability: 4,
+    power: 2
+  },
+  {
+    name: 'Yoshi',
+    speed: 2,
+    maneuverability: 4,
+    power: 3
+  },
+  {
+    name: 'Donkey Kong',
+    speed: 2,
+    maneuverability: 2,
+    power: 5
+  }
+];
+
+module.exports = { characters };
